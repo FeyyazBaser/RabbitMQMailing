508ffa7642a5fd1f7066a497b51c4895478b3912
feyyaz başer <feyaz12@hotmail.com>
2023-05-30 11:27:52 +0300

    Add project files.

---
+++ RabbitMQConfirmMail/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RabbitMQConfirmMail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
